7ef18ce9d76cb7bd89873c8b1725aacbe004ea54
Louis Choquel <louis@pipelex.com>
2025-11-13 17:01:58 +0100

    Demo materials

---
+++ examples.sh
@@ -0,0 +1,11 @@
+pipelex build pipe "Take a CV and Job offer in PDF, analyze if they match and generate 5 questions for the interview"
+
+pipelex build pipe "Imagine a cute animal mascot for a startup based on its elevator pitch and some brand guidelines"
+
+pipelex build pipe "Imagine a cute animal mascot for a startup based on its elevator pitch \
+    and some brand guidelines, propose 2 different ideas, and for each, 3 style variants in the image generation prompt, \
+        at the end we want the rendered image" -o mascot
+
+pipelex build pipe "Given an expense report, apply company rules"
+
+pipelex build pipe "Given a theme, write a Haiku"
